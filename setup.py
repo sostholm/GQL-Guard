4aeed827b1f09ca05af90961bd15852d579edc76
root <sostholm@gmail.com>
2022-01-11 18:20:38 +0000

    removing find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools  import setup, find_packages
+from setuptools  import setup
 
 setup(
     name='gql-guard',
     version='0.9',
-    packages=find_packages(),
+    packages=['gql_guard'],
     license='Apache 2.0',
     author='Samuel Ostholm',
     description="Provides a JWT token based access restrictions to graphql queries and mutations",
